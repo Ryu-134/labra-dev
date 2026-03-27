2a7de43d0b50b47ecd434cefb92988692caae2b0
Mark Gaballa <mgaballa@csu.fullerton.edu>
2026-03-26 18:30:33 -0700

    use random hash for state in oauth / use cookie to store state

--- labra-backend/internal/api/services/oauth.go
+++ labra-backend/internal/api/services/oauth.go
@@ -2,9 +2,12 @@ package services
 
 import (
 	"context"
+	"crypto/rand"
+	"encoding/base64"
 	"fmt"
 	"io"
 	"net/http"
+	"time"
 
 	"golang.org/x/oauth2"
 	"golang.org/x/oauth2/github"
@@ -25,23 +28,42 @@ func InitOauth(gh_client, gh_secret string) {
 	}
 }
 
-func Authenticate(w http.ResponseWriter, r *http.Request) {
+func Authenticate(w http.ResponseWriter, r *http.Request) error {
+	fmt.Println("CLIENT ID:", oauthConfig.ClientID)
 	verifier = oauth2.GenerateVerifier()
 
-	// TODO: generate a state, probably with the std hash lib
-	url := oauthConfig.AuthCodeURL("state", oauth2.S256ChallengeOption(verifier))
+	state, err := generateState()
+	if err != nil {
+		return err
+	}
+
+	cookieState := http.Cookie{
+		Name:    "oauthstate",
+		Value:   state,
+		Expires: time.Now().Add(30 * time.Second),
+	}
+
+	http.SetCookie(w, &cookieState)
+	url := oauthConfig.AuthCodeURL(state, oauth2.S256ChallengeOption(verifier))
 
 	http.Redirect(w, r, url, http.StatusTemporaryRedirect)
+
+	return nil
 }
 
 func Callback(w http.ResponseWriter, r *http.Request) ([]byte, error) {
 	ctx := context.Background()
 
 	code := r.URL.Query().Get("code")
-	_ = r.URL.Query().Get("state")
+	state := r.URL.Query().Get("state")
 
 	if code == "" {
 		return nil, fmt.Errorf("error: code is unavaliable")
+	}
+
+	oauthState, _ := r.Cookie("oauthstate")
+	if state != oauthState.Value {
+		return nil, fmt.Errorf("error: state does not match")
 	}
 
 	tok, err := oauthConfig.Exchange(ctx, code, oauth2.VerifierOption(verifier))
@@ -63,4 +85,15 @@ func Callback(w http.ResponseWriter, r *http.Request) ([]byte, error) {
 	}
 
 	return body, nil
+}
+
+func generateState() (string, error) {
+	state := make([]byte, 24)
+	fmt.Println("GEN")
+	if _, err := io.ReadFull(rand.Reader, state); err != nil {
+		return "", err
+	}
+	fmt.Println("COMPLETE")
+
+	return base64.StdEncoding.EncodeToString(state), nil
 }
